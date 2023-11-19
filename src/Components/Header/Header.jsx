fed09d73464b8329a2df4d5031dc9b1a073b7229
mahyarnafisi <mahyar.nafisi@gmail.com>
2023-11-19 13:39:23 +0100

    modified the navbar -add new icon from material ui icon library -install npm material UI icon library

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import Styles from "./Header.module.css";
+import KeyboardArrowDownRoundedIcon from "@mui/icons-material/KeyboardArrowDownRounded";
+import RestaurantMenuRoundedIcon from "@mui/icons-material/RestaurantMenuRounded";
+import ShoppingBasketRoundedIcon from "@mui/icons-material/ShoppingBasketRounded";
+import PersonRoundedIcon from "@mui/icons-material/PersonRounded";
+import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
 import brandLogo from "./../../assets/image/Logo .svg";
 import { Link, NavLink } from "react-router-dom";
 function Header() {
@@ -11,22 +16,37 @@ function Header() {
         </NavLink>
         <ul className={Styles.navbar__list}>
           <li className={Styles.navbar__item}>
-            <NavLink className={Styles.navbar__link}>Home</NavLink>
+            <NavLink className={Styles.navbar__link}>
+              <HomeRoundedIcon />
+              Home
+            </NavLink>
           </li>
           <li className={Styles.navbar__item}>
-            <Link className={Styles.navbar__link}>About</Link>
+            <Link className={Styles.navbar__link}>
+              About <KeyboardArrowDownRoundedIcon />
+            </Link>
           </li>
           <li className={Styles.navbar__item}>
-            <Link className={Styles.navbar__link}>Menu</Link>
+            <Link className={Styles.navbar__link}>
+              Menu <KeyboardArrowDownRoundedIcon />
+            </Link>
           </li>
           <li className={Styles.navbar__item}>
-            <NavLink className={Styles.navbar__link}>Reservation</NavLink>
+            <NavLink className={Styles.navbar__link}>
+              {" "}
+              <RestaurantMenuRoundedIcon /> Reservation
+            </NavLink>
           </li>
           <li className={Styles.navbar__item}>
-            <NavLink className={Styles.navbar__link}>Order online</NavLink>
+            <NavLink className={Styles.navbar__link}>
+              {" "}
+              <ShoppingBasketRoundedIcon /> Order online
+            </NavLink>
           </li>
           <li className={`${Styles.navbar__item} ${Styles.navbar__btnLogin}`}>
-            <NavLink className={Styles.navbar__link}>Login</NavLink>
+            <NavLink className={Styles.navbar__link}>
+              <PersonRoundedIcon /> Login
+            </NavLink>
           </li>
         </ul>
       </nav>
